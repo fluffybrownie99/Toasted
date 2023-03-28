94ea00e9c83a48929268414377fb2941736a16bc
Ramith <ramithr11@gmail.com>
2023-03-27 20:57:55 -0700

    Implemented classes and allows enemies to bounce around the screen

--- game.py
+++ game.py
@@ -1,84 +1,112 @@
 import pygame
-import os
+
+class Game:
+    def __init__(self):
+        # initialize pygame and create the window
+        pygame.init()
+        self.screen = pygame.display.set_mode((700, 900))
+        pygame.display.set_caption("Bullet Hell")
+
+        # set up the game objects
+        self.player = Player(self.screen)
+        self.enemies = [Enemy(self.screen, "./assets/pjar.png", (100, 100)), 
+                        Enemy(self.screen, "./assets/jellyjar.png", (200, 100)),
+                        Enemy(self.screen, "./assets/pjar.png", (300, 100)),
+                        Enemy(self.screen, "./assets/jellyjar.png", (400, 100))
+                        ]
+
+        # set up the clock and velocity
+        self.clock = pygame.time.Clock()
+        self.velocity = 10
 
 
-#Defining some colors to use and refer to at a later time
-BLACK = ( 0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-ANTIFLASHWHITE = (235, 235, 235)
-PUMPKIN = (255, 103, 0)
-SILVER = (192, 192, 192)
-BICEBLUE = (58, 110, 165)
-POLYNESIANBLUE = (0, 78, 152)
-main = True
-velocity = 10
+    def run(self):
+        # main game loop
+        while True:
+            # handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
 
-#Player Class
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, x, y, img):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join('images',img)).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+            # update the game objects
+            self.player.update()
 
-class Player(Sprite):
-    pass
+            for enemy in self.enemies:
+                enemy.update()
 
-class Jarheads(Sprite):
-    pass
+                if self.player.collides_with(enemy):
+                    pygame.quit()
+                    return
 
-#pygame setup
-pygame.init()
-screen = pygame.display.set_mode((700, 700))
-#sprite stuff
-toastysource = pygame.image.load("./assets/toastman.png").convert_alpha()
-jellysource = pygame.image.load("./assets/jellyjar.png").convert_alpha()
-peanutjarsource = pygame.image.load("./assets/pjar.png").convert_alpha()
-#resize source sprites
-toasty = pygame.transform.scale(toastysource, (100, 100))
-jelly = pygame.transform.scale(jellysource, (30,50))
-peanutjar = pygame.transform.scale(peanutjarsource, (30, 50))
+            # draw the game objects
+            self.screen.fill((58, 110, 165))
+            self.player.draw()
+            for enemy in self.enemies:
+                enemy.draw()
+            # update the screen
+            pygame.display.flip()
 
-pygame.display.set_caption("Bullet Hell")
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-pjarpos = pygame.Vector2(100, 100)
-jjarpos = pygame.Vector2(200, 200)
-pygame.display.flip()
-clock = pygame.time.Clock()
+            # limit the frame rate
+            self.clock.tick(60)
 
-#Main game loop
-while main:
-    enemy_vel = 10
-    enemy_vel += 1
-    #If game is exited
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            main = False
-    screen.fill(BICEBLUE)
-    #Controls
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        if player_pos.x > 0:
-            player_pos.x -= velocity#Moves left
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        if player_pos.x < 600:
-            player_pos.x += velocity#Moves right
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        if player_pos.y > 0:
-            player_pos.y -= velocity  #Moves up
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        if player_pos.y < 600:
-            player_pos.y += velocity  #Moves down
-    screen.blit(toasty, (player_pos.x, player_pos.y))
-    screen.blit(peanutjar, (enemy_vel, player_pos.y)), 60
-    screen.blit(jelly, (enemy_vel, player_pos.y+20)), 60
-    # #collision detection
-    # if pygame.sprite.spritecollide(toasty,peanutjar, True, None):
-    #     screen.fill(PUMPKIN)
-    pygame.display.flip()
-    clock.tick(60)
 
+class Player:
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("./assets/toastman.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect(center=(screen.get_width() / 2, screen.get_height() - 50))
+        self.velocity = 10
+
+    def update(self):
+        # handle player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if self.rect.left > 0:
+                self.rect.x -= self.velocity
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if self.rect.right < self.screen.get_width():
+                self.rect.x += self.velocity
+        # if keys[pygame.K_UP] or keys[pygame.K_w]:
+        #     if self.rect.top > 0:
+        #         self.rect.y -= self.velocity
+        # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        #     if self.rect.bottom < self.screen.get_height():
+        #         self.rect.y += self.velocity
+
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
+
+    def collides_with(self, other):
+        return self.rect.colliderect(other.rect)
+
+class Enemy:
+    def __init__(self, screen, image_path, position):
+        self.screen = screen
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 50))
+        self.rect = self.image.get_rect(topleft=position)
+        self.speed = [4,4]
+
+    def update(self):
+        if self.rect.left < 0 or self.rect.right > self.screen.get_width():
+            self.speed[0] = -self.speed[0]
+        if self.rect.top < 0 or self.rect.bottom > self.screen.get_height():
+            self.speed[1] = -self.speed[1]
+        
+        self.rect.move_ip(self.speed)
+        # elif self.rect.right < self.screen.get_width():
+        #     self.rect.x += self.velocity
+        # elif self.rect.top > 0:
+        #     self.rect.y -= self.velocity
+        # elif self.rect.bottom < self.screen.get_height():
+        #     self.rect.y += self.velocity
+    
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
