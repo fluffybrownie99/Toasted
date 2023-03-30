bc114e0721312c9223fe02fa10cba01424d4171d
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2023-03-30 16:14:23 -0700

    allows enemies to bounce off each other

--- game.py
+++ game.py
@@ -5,22 +5,24 @@ class Game:
     def __init__(self):
         # initialize pygame and create the window
         pygame.init()
-        self.screen = pygame.display.set_mode((700, 900))
+        self.screen = pygame.display.set_mode((500, 700))
         pygame.display.set_caption("Bullet Hell")
 
         # set up the game objects
         self.player = Player(self.screen)
         self.enemies = []
+        #for loop for declaring random x y coordinates for enemy spawns
         for i in range(2):
             x = random.randint(0, self.screen.get_width() - 30)
-            y = random.randint(0, self.screen.get_height() // 2)
+            y = random.randint(0, self.screen.get_height() // 4)
+            x2 = random.randint(0, self.screen.get_width() - 30)
+            y2 = random.randint(0, self.screen.get_height() // 4)
             self.enemies.append(Enemy(self.screen, "./assets/pjar.png", (x, y)))
-            self.enemies.append(Enemy(self.screen, "./assets/jellyjar.png", (y, x)))
+            self.enemies.append(Enemy(self.screen, "./assets/jellyjar.png", (x2, y2)))
 
 
         # set up the clock and velocity
         self.clock = pygame.time.Clock()
-        self.velocity = 10
 
 
     def run(self):
@@ -34,16 +36,24 @@ class Game:
 
             # update the game objects
             self.player.update()
-
             for enemy in self.enemies:
-                enemy.update()
-
-                if self.player.collides_with(enemy):
+                enemy.update(self.enemies)
+                
+            if self.player.collides_with(enemy):
+                print(enemy)
+                self.enemies.remove(enemy)
+                for goon in self.enemies:
+                    goon.speed[0] -= 2
+                    goon.speed[1] -= 2
+                    goon.update(self.enemies)
+                if self.player.health == 1:
                     pygame.quit()
                     return
-
+                else:
+                    self.player.health -= 1
+                    self.player.update()
             # draw the game objects
-            self.screen.fill((58, 110, 165))
+            self.screen.fill((58, 110, 165)) #RGB value that fills the background
             self.player.draw()
             for enemy in self.enemies:
                 enemy.draw()
@@ -60,6 +70,7 @@ class Player:
         self.image = pygame.image.load("./assets/toastman.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect(center=(screen.get_width() / 2, screen.get_height() - 50))
+        self.health = 3
         self.velocity = 10
         self.jump_height = 20
         self.jump_count = 0
@@ -106,22 +117,24 @@ class Enemy:
         self.image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 50))
         self.rect = self.image.get_rect(topleft=position)
-        self.speed = [4,4]
+        self.speed = [4, 4]
 
-    def update(self):
+    def update(self, enemies):
+        for enemy in enemies:
+            if enemy == self:
+                continue
+            if self.rect.colliderect(enemy.rect):
+                self.speed[0] = -self.speed[0]
+                self.speed[1] = -self.speed[1]
+                break
+
         if self.rect.left < 0 or self.rect.right > self.screen.get_width():
             self.speed[0] = -self.speed[0]
         if self.rect.top < 0 or self.rect.bottom > self.screen.get_height():
             self.speed[1] = -self.speed[1]
-        
+
         self.rect.move_ip(self.speed)
-        # elif self.rect.right < self.screen.get_width():
-        #     self.rect.x += self.velocity
-        # elif self.rect.top > 0:
-        #     self.rect.y -= self.velocity
-        # elif self.rect.bottom < self.screen.get_height():
-        #     self.rect.y += self.velocity
-    
+
     def draw(self):
         self.screen.blit(self.image, self.rect)
 
