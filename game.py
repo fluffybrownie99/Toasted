502998c2e83fd52ff410ddb113c0d1a57f5a4ea5
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2023-04-02 14:44:33 -0700

    adds all assets, a score function, and a menu screen(menu screen needs to be fixed

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame, random
 from enemy import Enemy
 from player import Player
+from screens.menu import MenuScreen
 
 class Game:
     def __init__(self):
@@ -15,9 +16,8 @@ class Game:
         min_distance = 80
         #for loop for declaring random x y coordinates for enemy spawns
         while len(self.enemies) < 9:
-            x = random.randint(0, self.screen.get_width() - 30)
+            x = random.randint(0, self.screen.get_width() - 40)
             y = random.randint(0, (self.screen.get_height() // 4)+ 10)
-
             #spawn collision detection to make sure enemies do not spawn on top of each other
             spawn_collision = False
             for enemy in self.enemies:
@@ -25,38 +25,44 @@ class Game:
                 distance = ((x - enemy.rect.centerx)**2 + (y - enemy.rect.centery)**2)**0.5
                 if distance < min_distance:
                     spawn_collision = True
-            random_enemy_png_path = ["./assets/pjar.png", "./assets/jellyjar.png", "./assets/brian.png", "./assets/shema.png"]
+            random_enemy_png_path = ["./assets/pjar.png", "./assets/jellyjar.png", "./assets/shema.png"]
             random_enemy_type = random.choice(random_enemy_png_path)
             if not spawn_collision:
                 self.enemies.append(Enemy(self.screen, random_enemy_type, (x, y)))
-        # set up the clock and velocity
+        # set up the clock and score
         self.clock = pygame.time.Clock()
-
+        self.score = 0
+        self.score_font = pygame.font.Font(None, 36)
+        self.menu_screen = MenuScreen(self.screen)
+        self.state = "menu"
 
     def run(self):
         # main game loop
+        start_time = pygame.time.get_ticks()
         while True:
             # handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return
-
             # update the game objects
             self.player.update()
             for enemy in self.enemies:
                 enemy.update(self.enemies)
-                
+            # update score based on elapsded time
+            elapsed_time = pygame.time.get_ticks() - start_time
+            self.score = elapsed_time // 10
+            
             collided_enemies = []
             for enemy in self.enemies:
                 if self.player.collides_with(enemy):
                     collided_enemies.append(enemy)
             for enemy in collided_enemies:
                 self.enemies.remove(enemy)
-
                 if self.player.health == 1:
-                    print(self.player.health)
-                    pygame.quit()
+                    final_score = self.score
+                    print(f"Final score: {final_score}")
+                    
                     return
                 else:
                     self.player.health -= 1
@@ -66,6 +72,9 @@ class Game:
             self.player.draw()
             for enemy in self.enemies:
                 enemy.draw()
+            #draw the score
+            score_text = self.score_font.render(f"Score: {self.score}", True, (255,255,255))
+            self.screen.blit(score_text, (10, 10))
             # update the screen
             pygame.display.flip()
 
---
+++ screens/menu.py
@@ -0,0 +1,48 @@
+import pygame
+
+class MenuScreen(pygame.Surface):
+    def __init__(self, window):
+        super().__init__(window.get_size())
+        self.screen = window
+        self.next_screen = None
+    
+    def menu(self):
+        font = pygame.font.Font(None, 80)
+        text = font.render("Toasted", True, (255, 255, 255))
+        text_rect = text.get_rect(center=(self.screen.get_width() // 2, 100))
+        
+        # Load the Toastman image
+        toastman_image = pygame.image.load("../assets/toastman.png").convert_alpha()
+        
+        # Get the width and height of the image
+        toastman_width, toastman_height = toastman_image.get_size()
+        
+        # Calculate the coordinates to center the image on the screen
+        toastman_x = (self.get_width() - toastman_width) // 2
+        toastman_y = (self.get_height() - toastman_height) // 2 - 50
+        
+        play_button = pygame.Rect(self.screen.get_width() // 2 - 100, self.screen.get_height() // 2 + 50, 200, 80)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if play_button.collidepoint(pygame.mouse.get_pos()):
+                        return
+            # Draw the menu screen
+            self.screen.fill((128, 0, 128)) #RGB Purple
+            play_button_color = (139, 69, 19)  # RGB value for brown color
+            play_button_text = font.render("PLAY", True, (255, 255, 255)) # text with white color
+            play_button_text_rect = play_button_text.get_rect(center=play_button.center)
+            pygame.draw.rect(self.screen, play_button_color, play_button)
+            self.screen.blit(play_button_text, play_button_text_rect)
+            pygame.draw.rect(self.screen, (255, 255, 255), play_button, 2)
+            play_button = pygame.Rect(self.screen.get_width() // 2 - 100, self.screen.get_height() // 2 + 150, 200, 80)
+            # Blit the Toastman image onto the screen
+            self.screen.blit(toastman_image, (toastman_x, toastman_y))
+            
+            # Blit the "Toasted" text onto the screen
+            self.blit(text, text_rect)
+            
+            pygame.display.flip()
