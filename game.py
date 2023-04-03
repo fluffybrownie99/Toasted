eb7e94fe2425b7e90a99b4970ff4122859476074
Ramith <ramithr11@gmail.com>
2023-04-02 18:34:08 -0700

    refactors code and made it more readable

--- game.py
+++ game.py
@@ -10,10 +10,12 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((500, 700))
         pygame.display.set_caption("Bullet Hell")
-
         # Set up the player object
         self.player = Player(self.screen)
-        # Set up the enemies object with more logic
+        # Set up the player's health with a heart image
+        self.heart_image = pygame.image.load("./assets/heart.png")
+        self.heart = pygame.transform.scale(self.heart_image, (30, 30))
+        # Set up the enemy list to load with the get_enemies() function
         self.enemies = []
         # Set up the clock and score
         self.clock = pygame.time.Clock()
@@ -23,9 +25,11 @@ class Game:
         self.state = "menu"
     
     def get_enemies(self):
-        self.enemies = []
-        margin = 40
-        min_distance = 80
+    # This function initializes the enemies list with enemies that are placed randomly 
+        self.enemies = [] #this is for clearing the list after trying the game again
+        margin = 40 #this is for making sure the enemies spawn within the margins of the display
+        min_distance = 80 #this is so that the enemies have a minimum distance of 80 between spawning
+        random_enemy_png_paths = ["./assets/pjar.png", "./assets/jellyjar.png", "./assets/shema.png"]
         # While loop for declaring number of enemy spawns with random coordinates for each spawn
         while len(self.enemies) < 9:
             x = random.randint(0, (self.screen.get_width() - margin))
@@ -37,19 +41,41 @@ class Game:
                 distance = ((x - enemy.rect.centerx)**2 + (y - enemy.rect.centery)**2)**0.5
                 if distance < min_distance:
                     spawn_collision = True
-            random_enemy_png_path = ["./assets/pjar.png", "./assets/jellyjar.png", "./assets/shema.png"]
-            random_enemy_type = random.choice(random_enemy_png_path)
+            random_enemy_type = random.choice(random_enemy_png_paths)
             if not spawn_collision:
                 self.enemies.append(Enemy(self.screen, random_enemy_type, (x, y)))
     
+    
+    def draw_hearts(self):
+    #this function is for drawing the hearts on the screen that represents the player health
+        for i in range(self.player.health):
+            x_position = 10 + (i * (self.heart.get_width()+ 10))
+            self.screen.blit(self.heart, (x_position, 50))
+    
+    def handle_quit(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+    def handle_collisions(self):
+        #handles collisions and player health based on collision detection
+        collided_enemies = [enemy for enemy in self.enemies if self.player.collides_with(enemy)]
+        for enemy in collided_enemies:
+            self.enemies.remove(enemy)
+            if self.player.health == 1:
+                return self.score
+            else:
+                self.player.health -= 1
+                self.player.update()
+        return None
+    
+
     def run(self):
         # Main game loop
         while True:
             # Handle exit events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    return
+            self.handle_quit()
+            # Handles game states
             if self.state =="menu":
                 self.menu_screen.menu()
                 self.state = "play"
@@ -60,31 +86,28 @@ class Game:
                 game_over_screen = GameOverScreen(self.screen, final_score)
                 game_over_screen.game_over()
                 self.state = "menu"
+            
             # update the game objects
             self.player.update()
             for enemy in self.enemies:
                 enemy.update(self.enemies)
+                
             # update score based on elapsded time
             elapsed_time = pygame.time.get_ticks() - start_time
             self.score = elapsed_time // 10
             
-            collided_enemies = []
-            for enemy in self.enemies:
-                if self.player.collides_with(enemy):
-                    collided_enemies.append(enemy)
-            for enemy in collided_enemies:
-                self.enemies.remove(enemy)
-                if self.player.health == 1:
-                    final_score = self.score
-                    self.state = "game_over"
-                else:
-                    self.player.health -= 1
-                    self.player.update()
-            # draw the game objects
+            #handle collisions and player health with self.handle_collisions
+            final_score = self.handle_collisions()
+            if final_score is not None:
+                self.state = "game_over"
+            
+            # draw the game background and objects
             self.screen.fill((58, 110, 165)) #RGB value that fills the background
             self.player.draw()
             for enemy in self.enemies:
                 enemy.draw()
+            # draws hearts on the screen
+            self.draw_hearts()
             #draw the score
             score_text = self.score_font.render(f"Score: {self.score}", True, (255,255,255))
             self.screen.blit(score_text, (10, 10))
